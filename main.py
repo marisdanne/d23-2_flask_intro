f07ef674e96b6c41ddd4ce8588a93437b5228a52
Māris Danne <Maris.Danne@rvt.lv>
2020-02-11 07:55:24 +0200

    add last

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from file_proc import read_file, write_file
 
 app = Flask(__name__)
 
@@ -8,25 +9,47 @@ def index():
 
 @app.route('/home')
 def home():
-  return render_template('home.html', aktiva_lapa = 'home')
+  return render_template('home.html')
 
 @app.route('/about')
-def about():
+def about_html():
   return render_template('about.html')
 
 @app.route('/contact')
-def contact():
-  #info no datubazes
-  return render_template('contact.html', phone = 22140077)
+def contacts_html():
+  return render_template('contact.html', phone = "22140077")
 
 @app.route('/params')
 def params():
   return request.args
 
-@app.route('/post_req', methods = ['POST'])
-def post_req():
-  return request.args
+@app.route('/post', methods = ['POST'])
+def post():
+  return request.get_json()
 
+@app.route('/read_file')
+def read_from_file():
+  content = read_file()
+  return content
+
+@app.route('/write_file', methods = ['POST'])
+def write_to_file():
+  content_type = request.content_type
+  if content_type == 'application/json':
+    contentJSON = request.get_json()
+    write_file(contentJSON['data'])
+    return f"Add line {contentJSON['data']} to file."
+  else:
+    return f"Content type {content_type} is not supported!"
+  
+@app.route('/file', methods = ['GET', 'POST'])
+def workFile():
+  if request.method == 'GET':
+    return read_from_file()
+  elif request.method == 'POST':
+    return write_to_file()
+  else:
+    return f"Method {request.method} is not supported!"
 
 if __name__ == '__main__':
-  app.run(host = '0.0.0.0', port = 5232, threaded = True, debug = True)
+  app.run(host="0.0.0.0", threaded=True, port=5020, debug=True)
